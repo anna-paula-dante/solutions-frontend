b514646ca443a92f1e7aa2d3a98c1f8b52899cd6
Ana Paula <anajornalistasbc@gmail.com>
2025-08-19 10:27:28 -0300

    Feat: Solução teste 01

--- src/Desafio1/index.tsx
+++ src/Desafio1/index.tsx
@@ -3,12 +3,17 @@
   de errado e também a solução que você encontrou.
 
   Resposta / comentários sobre a sua solução:
-
+  Componentes react precisam retornar apenas 1 elemento
+  a partir do momento que se tem mais de um elemento e preciso
+  utilizar um elemento de encapsulamento como: div, header, section.
+  Ou um fragment que não adicionaria um elemento mas envolveria os que o componente possui(<> </>).
 */
 
-export const Desafio1 = ()=> {
+export const Desafio1 = () => {
   return (
+    <>
       <span>item 1</span>
       <span>item 2</span>
-  )
-}
+    </>
+  );
+};
