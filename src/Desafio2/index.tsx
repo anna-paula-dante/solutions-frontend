05e55fbe57ebe3569bd5da1ae2e1fed831f3cdb9
Ana Paula <anajornalistasbc@gmail.com>
2025-08-19 10:27:50 -0300

    Feat: Solução teste 02

--- src/Desafio2/index.tsx
+++ src/Desafio2/index.tsx
@@ -1,17 +1,23 @@
-import {useState} from 'react';
+import { useState } from "react";
 
 /* TODO
   O componente abaixo está com problemas. Resolva o problema e explique o que tinha
   de errado e também a solução que você encontrou.
 
   Resposta / comentários sobre a sua solução:
-
+  componentes react que utilizam array.map precisam que cadda elemento que o map retorne 
+  possua uma key (string ou numero unico do elemento) que o identifique exclusivamente entre os outros
+  itens.
 */
 
-export const Desafio2 = ()=> {
-  const [list, setList] = useState([
-    1, 2
-  ]);
+export const Desafio2 = () => {
+  const [list, setList] = useState([1, 2]);
 
-  return (<ul>{list.map(item => <li>item: {item}</li>)}</ul>)
-}
+  return (
+    <ul>
+      {list.map((item) => (
+        <li key={item}>item: {item}</li>
+      ))}
+    </ul>
+  );
+};
